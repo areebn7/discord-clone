1cd7374b045620d646c7a14f3f146f08a32139e2
Areeb Nabi <anabi001@odu.edu>
2023-12-16 19:32:40 -0500

    Create authentication using clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="">
-      <p className="text-3xl font-bold text-indigo-500">Hello World :D</p>
-      <Button>Click me</Button>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
     </div>
   );
 }
